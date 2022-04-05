86c189580be4406e7e5d5663aa0d0dd52dcebf95
AmandaCristine <amandacris.sb@gmail.com>
2022-04-05 11:51:24 -0300

    Adicionando cadastrar produtos

---
+++ src/componentes/produtos/cadastarProdutos/CadastrarProdutos.tsx
@@ -0,0 +1,139 @@
+import React, { ChangeEvent, useEffect, useState } from 'react'
+import { Container, Typography, TextField, Button, Select, InputLabel, MenuItem, FormControl, FormHelperText } from "@material-ui/core"
+import './CadastroPost.css';
+import { useHistory, useParams } from 'react-router-dom';
+import Categoria from '../../../models/Categoria';
+import useLocalStorage from 'react-use-localstorage';
+import Produto from '../../../models/Produto';
+import { busca, buscaId, post, put } from '../../../services/Service';
+
+function CadastrarProdutos() {
+    let history = useHistory();
+    const { id } = useParams<{ id: string }>();
+    const [categorias, setCategorias] = useState<Categoria[]>([])
+    const [token, setToken] = useLocalStorage('token');
+
+    useEffect(() => {
+        if (token == "") {
+            alert("Você precisa estar logado")
+            history.push("/login")
+
+        }
+    }, [token])
+
+    const [categoria, setCategoria] = useState<Categoria>(
+        {
+            id: 0,
+            materia: '',
+            areaDeEstudo: ''
+        })
+    const [produto, setProduto] = useState<Produto>({
+        id: 0,
+        nomeMateria: '',
+        descricao: '',
+        agenda: '',
+        valor: 0,
+        categoria: null
+    })
+
+    useEffect(() => { 
+        setProduto({
+            ...produto,
+            categoria: categoria
+        })
+    }, [categoria])
+
+    useEffect(() => {
+        getCategorias()
+        if (id !== undefined) {
+            findByIdProduto(id)
+        }
+    }, [id])
+
+    async function getCategorias() {
+        await busca("/categoria", setCategorias, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    async function findByIdProduto(id: string) {
+        await buscaId(`produto/${id}`, setProduto, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    function updatedProduto(e: ChangeEvent<HTMLInputElement>) {
+
+        setProduto({
+            ...produto,
+            [e.target.name]: e.target.value,
+            categoria: categoria
+        })
+
+    }
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        if (id !== undefined) {
+            put(`/produto`, produto, setProduto, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+            alert('Produto atualizado com sucesso');
+        } else {
+            post(`/produto`, produto, setProduto, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+            alert('Produto cadastrado com sucesso');
+        }
+        back()
+
+    }
+
+    function back() {
+        history.push('/produtos')
+    }
+
+    return (
+        <Container maxWidth="sm" className="topo">
+            <form onSubmit={onSubmit}>
+                <Typography variant="h3" color="textSecondary" component="h1" align="center" >Formulário de cadastro postagem</Typography>
+                <TextField value={produto.nomeMateria} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedProduto(e)} id="nomeMateria" label="nomeMateria" variant="outlined" name="nomeMateria" margin="normal" fullWidth />
+                <TextField value={produto.descricao} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedProduto(e)} id="descricao" label="descricao" variant="outlined" name="descricao" margin="normal" fullWidth />
+                <TextField value={produto.agenda} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedProduto(e)} id="agenda" label="agenda" variant="outlined" name="agenda" margin="normal" fullWidth />
+                <TextField value={produto.valor} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedProduto(e)} id="valor" label="valor" variant="outlined" name="valor" margin="normal" fullWidth />
+
+                <FormControl >
+                    <InputLabel id="demo-simple-select-helper-label">Categoria </InputLabel>
+                    <Select
+                        labelId="demo-simple-select-helper-label"
+                        id="demo-simple-select-helper"
+                        onChange={(e) => buscaId(`/categoria/${e.target.value}`, setCategoria, {
+                            headers: {
+                                'Authorization': token
+                            }
+                        })}>
+                        {
+                            categorias.map(categoria => (
+                                <MenuItem value={categoria.id}>{categoria.materia}</MenuItem>
+                            ))
+                        }
+                    </Select>
+                    <FormHelperText>Escolha uma categoria para o produto</FormHelperText>
+                    <Button type="submit" variant="contained" color="primary">
+                        Finalizar
+                    </Button>
+                </FormControl>
+            </form>
+        </Container>
+    )
+}
+export default CadastrarProdutos;
